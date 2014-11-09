82c315512e501dcedd25229725e59bfc734d89c4
gururea <iam@gururea.me>
2014-11-09 18:02:53 -0500

    Functions Definitions

---
+++ biopif.py
@@ -0,0 +1,25 @@
+__author__ = 'gururea'
+import sys
+import os
+from Bio import Seq
+
+def create_profile_hmm():
+    return 0
+
+def find_protein_domain():
+    return 0
+
+def parser_protein_domain():
+    return 0
+
+def draw_protein_domain():
+    return 0
+
+def find_homology_protein():
+    return 0
+
+def generate_csv():
+    return 0
+
+def save_data_MongoDB():
+    return 0
